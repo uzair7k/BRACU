7727f2b8ffc8605ce1a79986de2cc020c0b462d2
uzair <uzairahmed7k@gmail.com>
2026-07-18 02:09:57 +0600

    Create UnderstandingErrors.java

---
+++ CSE110 - Programming Language I/Worksheets/UnderstandingErrors.java
@@ -0,0 +1,11 @@
+public class UnderstandingErrors {
+    public static void main(String[] args) {
+        int x = 5/5;
+        int y = 7;
+        x -= 1;
+        y += 1;
+        System.out.println(y%2!=0);
+        int m = Integer.valueOf("15");
+        System.out.println(m);
+    }
+}
