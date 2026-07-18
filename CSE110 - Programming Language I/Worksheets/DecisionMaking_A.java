edb73d71801eec1c4fa450d4505d03104172dd07
uzair <uzairahmed7k@gmail.com>
2026-07-18 11:42:43 +0600

    Create DecisionMaking_A.java

---
+++ CSE110 - Programming Language I/Worksheets/DecisionMaking_A.java
@@ -0,0 +1,12 @@
+class DecisionMaking_A {
+    public static void main(String[]args) {
+        int a  = 120;
+        if (a > 100) {
+            System.out.println("A is greater than 100");
+        } else if (a > 10) {
+            System.out.println("A is greater than 10");
+        } else {
+            System.out.println("A is less than 10");
+        }
+    }
+}
