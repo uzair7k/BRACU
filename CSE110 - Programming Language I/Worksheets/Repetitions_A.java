bab7efd3778e4be1fdf09efaef210e05c5eb2450
uzair <uzairahmed7k@gmail.com>
2026-07-18 17:12:14 +0600

    Update Repetitions_A.java

--- CSE110 - Programming Language I/Worksheets/Repetitions_A.java
+++ CSE110 - Programming Language I/Worksheets/Repetitions_A.java
@@ -1,11 +1,16 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int n = 1;
-        int sum=0;
-        while (n <= 100) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Amount: ");
+        int N = sc.nextInt();
+        double sum = 0;
+        System.out.println("Values: ");
+        for (int i = 1; i <= N; i++) {
+            int n = sc.nextInt();
             sum += n;
-            n++;
         }
-        System.out.println(sum);
+        double avg = sum / (N *1.0);
+        System.out.println("Average: " + avg);
     }
 }
