9423ccdfd6e30437fd101afac714bc9eaa9fd7c2
uzair <uzairahmed7k@gmail.com>
2026-07-18 11:46:21 +0600

    Create DecisionMaking_C.java

---
+++ CSE110 - Programming Language I/Worksheets/DecisionMaking_C.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class DecisionMaking_C {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter your number: ");
+      int n = sc.nextInt();
+      if (n >= 0) {
+          System.out.println(n);
+      } else {
+          System.out.println(-n);
+      }
+    }
+}   
