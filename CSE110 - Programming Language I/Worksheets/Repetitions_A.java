fe636d8c3101b556698494a89aecf9534af274b6
uzair <uzairahmed7k@gmail.com>
2026-07-18 16:52:04 +0600

    Create Repetitions_A.java

---
+++ CSE110 - Programming Language I/Worksheets/Repetitions_A.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 1;
+        int sum=0;
+        while (n <= 100) {
+            sum += n;
+            n++;
+        }
+        System.out.println(sum);
+    }
+}
