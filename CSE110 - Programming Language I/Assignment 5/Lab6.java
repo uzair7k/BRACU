50d9e095a48f11d473825ab5afef1734b11ff309
uzair <uzairahmed7k@gmail.com>
2026-08-06 10:11:38 +0600

    Update Lab6.java

--- CSE110 - Programming Language I/Assignment 5/Lab6.java
+++ CSE110 - Programming Language I/Assignment 5/Lab6.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 public class Lab6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int rows = sc.nextInt();
-        int cols = sc.nextInt();
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= cols; j++) {
-                if (i == 1 || i == rows || j == 1 || j == cols) {
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i == 1 || i == n || j == 1 || j == m) {
                     System.out.print(j);
                 } else {
                     System.out.print(" ");
