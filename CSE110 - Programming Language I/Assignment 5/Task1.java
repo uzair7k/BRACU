74071463c14247c89c96a2abe8242aa5bc3f3c09
uzair <uzairahmed7k@gmail.com>
2026-08-07 16:52:22 +0600

    Create Task1.java

---
+++ CSE110 - Programming Language I/Assignment 5/Task1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
