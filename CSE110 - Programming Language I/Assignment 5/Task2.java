0c926317f5cf0a4dc08b3ee0d39cb7ffd07caa59
uzair <uzairahmed7k@gmail.com>
2026-08-09 17:11:19 +0600

    Create Task2.java

---
+++ CSE110 - Programming Language I/Assignment 5/Task2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Lab4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                int val = n - i + j;
+                if (i == 1 || i == n || j == 1 || j == i) {
+                    System.out.print(val);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
