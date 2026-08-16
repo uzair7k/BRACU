28f7faa7a2b5aa80824daceb6d19fe41fb13dee3
uzair <uzairahmed7k@gmail.com>
2026-08-16 16:08:31 +0600

    Create Ungraded3.java

---
+++ CSE110 - Programming Language I/Assignment 5/Ungraded3.java
@@ -0,0 +1,25 @@
+public class Ungraded3 {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= 5; j++) {
+                if (j == i) {
+                    System.out.print("....");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = 4; i >= 2; i--) {
+            for (int j = 1; j <= 5; j++) {
+                if (j == i) {
+                    System.out.print("****");
+                } else {
+                    System.out.print(".");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+}
