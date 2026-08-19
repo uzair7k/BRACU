8c638f96c7525391fd01daad81c1ddf4060d2f9a
uzair <uzairahmed7k@gmail.com>
2026-08-19 09:19:10 +0600

    Create Strings_H.java

---
+++ CSE110 - Programming Language I/Worksheets/Strings_H.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class String_H {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        System.out.println(word);
+        System.out.println(word + "==THE END==");
+        System.out.println(word);
+    }
+}
