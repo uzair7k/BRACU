3fa2fa8927db872b8709fc63358d76888571d4ef
uzair <uzairahmed7k@gmail.com>
2026-09-12 10:04:17 +0600

    Create Task4.java

---
+++ CSE110 - Programming Language I/Assigntment 9/Task4.java
@@ -0,0 +1,11 @@
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println(fibonacci(9));        
+    }
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
